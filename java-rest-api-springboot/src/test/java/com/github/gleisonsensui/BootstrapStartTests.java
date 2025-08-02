fa28467b04e115be51dd5bfbeb9b723c3f9ceed6
gleisonsensui <gleison.nsilva@hotmail.com>
2025-08-02 01:24:51 -0300

    start api project

---
+++ java-rest-api-springboot/src/test/java/com/github/gleisonsensui/BootstrapStartTests.java
@@ -0,0 +1,13 @@
+package com.github.gleisonsensui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootstrapStartTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
