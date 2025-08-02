35c1d95ae93f84edc2ec4132668d0f8c8c1a600f
gleisonsensui <gleison.nsilva@hotmail.com>
2025-08-02 15:12:44 -0300

    adjusted project structure

---
+++ java-rest-api-springboot/src/main/java/com/github/gleisonsensui/BootstrapperStart.java
@@ -0,0 +1,13 @@
+package com.github.gleisonsensui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapperStart {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrapperStart.class, args);
+	}
+
+}
